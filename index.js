1392caa07e074b37f2c19ade0feb743646d3e52b
Andres Vizuete <andresv0807@gmail.com>
2024-06-29 08:30:15 -0500

    actualizacion del packaje.json

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-    import express from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
 import veterinarioRoutes from './routes/veterinarioRoutes.js'
 import pacienteRoutes from './routes/pacienteRoutes.js'
